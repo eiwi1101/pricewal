0834fc02cb7da73b6108982365b9c9d9061093e8
William Eisert <weisert@eisertdev.com>
2015-09-28 22:47:53 +0000

    Added user avatars.

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,8 @@
 class User < ActiveRecord::Base
+
+  has_attached_file :avatar, styles: { medium: "300x300>", thumb: "40x40" }, default_url: '/assets/avatar.png'
+  validates_attachment_content_type :avatar, content_type: /\Aimage\/.*\Z/
+
   validates_format_of :username,
     with:    /\A[A-Za-z0-9]+\z/,
     message: "can only contain letters, numbers and hyphens (-)"
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150807035452) do
+ActiveRecord::Schema.define(version: 20150928224645) do
 
   create_table "pages", force: true do |t|
     t.string   "title"
@@ -29,6 +29,10 @@ ActiveRecord::Schema.define(version: 20150807035452) do
     t.datetime "created_at"
     t.datetime "updated_at"
     t.boolean  "admin"
+    t.string   "avatar_file_name"
+    t.string   "avatar_content_type"
+    t.integer  "avatar_file_size"
+    t.datetime "avatar_updated_at"
   end
 
 end
