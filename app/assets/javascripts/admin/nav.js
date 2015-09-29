65fea38c1e84e1d41381d070d25dfe5d475584c1
William Eisert <weisert@eisertdev.com>
2015-09-29 23:22:24 +0000

    Fixed accordion nav feature. TODO: Click open nav item.

--- app/assets/javascripts/admin/nav.js
+++ app/assets/javascripts/admin/nav.js
@@ -3,7 +3,14 @@ $(document).on("ready page:load", function() {
     
     // Add Accordion Feature
     $('ul.accordion-nav li a').click(function() {
-      $(this).siblings('ul').toggle(200);
+      // Collapse All
+      $('ul.accordion-nav li').each(function() {
+        $(this).children('ul').slideUp(200);
+        $(this).children('a').children('.collapse-icon').removeClass('fa-chevron-down').addClass('fa-chevron-right');
+      });
+
+      // Expand or collapse current
+      $(this).siblings('ul').slideDown(200);
       $(this).children('.collapse-icon').toggleClass('fa-chevron-down').toggleClass('fa-chevron-right');
     });
   }
