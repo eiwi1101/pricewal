ee7a7a21197dde2e16ff7d8f83e9145103ed938a
William Eisert <weisert@eisertdev.com>
2015-10-24 22:07:13 +0000

    Software Revision available in Production (non-hg) mode from .hg_archival.txt

--- config/environment.rb
+++ config/environment.rb
@@ -5,8 +5,15 @@ require File.expand_path('../application', __FILE__)
 START_TIME = Time.now
 
 # Version
-    VERSION = "0.0"
- HG_VERSION = `hg log -r . --template '{latesttag}.{latesttagdistance}-{node|short}'`
+VERSION = "0.0"
+
+HG_VERSION = if File.exists? '.hg_archival.txt'
+  arc = YAML.load_file('.hg_archival.txt')
+  "#{arc['latesttag']}.#{arc['latesttagdistance']}-#{arc['node'][0,16]}"
+else
+  `hg log -r . --template '{latesttag}.{latesttagdistance}-{node|short}'`
+end
+
 CAP_VERSION = File.read("REVISION") rescue "DEV_BUILD"
 
 # Initialize the Rails application.
